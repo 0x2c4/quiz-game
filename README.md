2007c84f06b2834b7b7ab8582cba0e6cff15eceb
seven <j7r98c1j@wver345t5ty6hyrt.anonaddy.com>
2024-11-02 17:12:01 +0000

    initial version v0.1

---
+++ README.md
@@ -0,0 +1,15 @@
+## Console version of a Quic Game.
+To start the game, in root folder type:
+```bash
+python main.py
+```
+
+You can update the question list by updating the ```data.py``` file, just 
+follow the same structure of the dictionary in it:
+```
+{"text": "A slug's blood is green.", "answer": "True"}
+```
+Where the 'text' key will hows the question, and the 'answer' key will
+hold the correct answer.
+
+**Enjoy!**
--- quiz_brain.py
+++ quiz_brain.py
@@ -1,9 +1,27 @@
 class QuizBrain:
     def __init__(self, question_list):
         self.question_number = 0
+        self.score = 0
         self.question_list = question_list
+
+    def still_has_questions(self):
+        max_number_of_questions = len(self.question_list)
+        return self.question_number < max_number_of_questions
 
     def next_question(self):
         question = self.question_list[self.question_number]
         self.question_number += 1
-        input(f"Q.{self.question_number}: {question.text}. (True/False):  ")
+        user_answer = input(f"Q.{self.question_number}: {question.text}. (True/False):  ")
+        self.check_answer(user_answer, question.answer)
+
+    def check_answer(self, user_answer, correct_answer):
+        if user_answer.lower() == correct_answer.lower():
+            print("You got it right!")
+        else:
+            print("That's wrong.")
+        print("Correct answer:", correct_answer)
+
+    def score(self):
+        if self.check_answer():
+            self.score += 1
+        print(f"Your current score is {score}/{self.question_number}.")
